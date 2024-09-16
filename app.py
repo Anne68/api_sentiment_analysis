2d67c0799f17e26db434f3aad0196daaeae02413
Anne Atse <vicky69200@gmail.com>
2024-09-16 12:50:32 +0200

    modification app.py pour url azure

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from io import StringIO
 
 # URL de l'API FastAPI
-api_url = "https://sentimentanalysisgi-esbtjdewrdqcck76ovdbuf.streamlit.app/"  # Modifiez si nécessaire
+api_url = "https://streamlitsentiment-e9dpc6graxehejhb.francecentral-01.azurewebsites.net"  # Modifiez si nécessaire
 
 st.title("Analyse de Sentiment")
 
